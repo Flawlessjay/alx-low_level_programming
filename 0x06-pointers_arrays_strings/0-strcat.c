70c7c3c06b1218ddfdbafe83b922122c786460e5
flawlessjay <okeluejesse572@gmail.com>
2022-12-21 23:13:44 -0800

    task 0 pointer

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,7 @@
  * Return: pointer to designated string
  */
 
-char *_strcat(char dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int length, j;
 
@@ -23,6 +23,6 @@ char *_strcat(char dest, char *src)
 		dest[length] = src[j];
 	}
 
-	dest[length] = '\0';
+	dest[length] = src[j];
 	return (dest);
 }
