e2350066e81225e33765d1fdd05ac482669d6b92
flawlessjay <okeluejesse572@gmail.com>
2023-01-02 16:06:29 -0800

    memset pointer12

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
  * _memset - fills memory with a constant byte.
  * fills the first n bytes of the memory area pointed
  * to by s with the constant b.
- * pointer to memory area.
+ * @s: pointer to memory area.
  * @b: constant to fill memory with.
  * @n: bytes of the memory area to be filled.
  *
